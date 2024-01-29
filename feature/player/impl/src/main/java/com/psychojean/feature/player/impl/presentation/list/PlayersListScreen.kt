4598f75964c98d4099a591170e91c3111566a391
RasulMingazov <mingazovri1@sovcombank.ru>
2024-01-29 03:36:23 +0300

    Temporarily hide the display of players

--- feature/player/impl/src/main/java/com/psychojean/feature/player/impl/presentation/list/PlayersListScreen.kt
+++ feature/player/impl/src/main/java/com/psychojean/feature/player/impl/presentation/list/PlayersListScreen.kt
@@ -1,14 +1,13 @@
 package com.psychojean.feature.player.impl.presentation.list
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemKey
 
 @Composable
 internal fun PlayersListScreen(
@@ -18,10 +17,14 @@ internal fun PlayersListScreen(
 ) {
     val players = viewModel.players.collectAsLazyPagingItems()
 
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(count = players.itemCount, key = players.itemKey { it.id }) { index ->
-            val player = players[index]
-            Text(modifier = Modifier.clickable { onDetailClick(player?.id ?: -1) }, text = player?.fullName ?: "")
-        }
-    }
+    Text(text = "Test", Modifier.padding(top = 44.dp).clickable {
+        onDetailClick(133)
+    })
+
+//    LazyColumn(modifier = modifier.fillMaxSize()) {
+//        items(count = players.itemCount, key = players.itemKey { it.id }) { index ->
+//            val player = players[index]
+//            Text(modifier = Modifier.clickable { onDetailClick(player?.id ?: -1) }, text = player?.fullName ?: "")
+//        }
+//    }
 }
